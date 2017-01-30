7a8a5fde1feefe18f7684d472a2af66ab822cc03
Vadim Solovey <vadim@doit-intl.com>
2017-01-30 22:55:09 +0200

    Update setup-emr.sh

--- scripts/setup-emr.sh
+++ scripts/setup-emr.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-f [ $# -ne 1 ]
-then
-echo "USAGE: emr-setup.sh student-name"
-return
+if [ $# -eq 0 ]; then
+    echo "USAGE: emr-setup.sh student-name"
+    exit 1
 fi
 
 student=$1
 
 echo Setting up EMR cluster for student $student
 
-aws emr create-cluster --name emr-$student --auto-scaling-role EMR_AutoScaling_DefaultRole --applications \
-    Name=Hadoop Name=Hive Name=Pig Name=Hue Name=Spark Name=Presto \
-    --ec2-attributes '{"KeyName":"vadim_dahouse","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-10477259","EmrManagedSlaveSecurityGroup":"sg-1ffc5363","EmrManagedMasterSecurityGroup":"sg-23fc535f"}' \
-    --service-role EMR_DefaultRole --enable-debugging --release-label emr-5.3.0 \
+aws emr create-cluster --name emr-$student \
+    --applications Name=Hadoop Name=Hive Name=Pig Name=Hue Name=Spark Name=Presto \
+    --ec2-attributes '{"KeyName":"$student-key","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-10477259","EmrManagedSlaveSecurityGroup":"sg-1ffc5363","EmrManagedMasterSecurityGroup":"sg-23fc535f"}' \
+    --service-role EMR_DefaultRole --enable-debugging --release-label emr-5.3.0 --region us-east-1 \
     --log-uri 's3n://dahouse-emr-logs/$student/' \
     --instance-groups '[{"InstanceCount":2,"InstanceGroupType":"CORE","InstanceType":"m3.xlarge","Name":"Core - 2"},{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m3.xlarge","Name":"Master - 1"}]' \
-    --scale-down-behavior TERMINATE_AT_INSTANCE_HOUR --region us-east-1
