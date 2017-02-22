3db36412baf9b2d056b76d9d94b2ccaf64dff4f6
Spark 3K <spark3k@spark3k.local>
2017-02-22 15:42:15 +0200

    fixing key pair problem when starting new emr cluster

--- scripts/setup-emr.sh
+++ scripts/setup-emr.sh
@@ -11,7 +11,7 @@ echo Setting up EMR cluster for student $student
 
 aws emr create-cluster --name emr-$student \
     --applications Name=Hadoop Name=Hive Name=Pig Name=Hue Name=Spark Name=Presto \
-    --ec2-attributes '{"KeyName":"$student-key","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-10477259","EmrManagedSlaveSecurityGroup":"sg-1ffc5363","EmrManagedMasterSecurityGroup":"sg-23fc535f"}' \
-    --service-role EMR_DefaultRole --enable-debugging --release-label emr-5.3.0 --region us-east-1 \
+    --ec2-attributes '{"KeyName":"'$student'","InstanceProfile":"EMR_EC2_DefaultRole","SubnetId":"subnet-10477259","EmrManagedSlaveSecurityGroup":"sg-1ffc5363","EmrManagedMasterSecurityGroup":"sg-23fc535f"}' \
+    --service-role EMR_DefaultRole --enable-debugging --release-label emr-5.3.1 --region us-east-1 \
     --log-uri 's3n://dahouse-emr-logs/$student/' \
     --instance-groups '[{"InstanceCount":2,"InstanceGroupType":"CORE","InstanceType":"m3.xlarge","Name":"Core - 2"},{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m3.xlarge","Name":"Master - 1"}]' \
